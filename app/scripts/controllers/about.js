f20499b68d5f9a100eb8ab41524048b54d435221
dlwhitehurst <dlwhitehurst@me.com>
2016-01-15 20:42:31 -0500

    initial check-in

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciwiseInvoicerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ciwiseInvoicerApp
+ */
+angular.module('ciwiseInvoicerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
